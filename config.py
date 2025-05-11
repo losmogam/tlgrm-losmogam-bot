f9e8934468b4cad0579c50bc6106c03bbee7dedd
losmogam <losmogamsw@gmail.com>
2025-05-11 18:59:01 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 TELEGRAM_TOKEN = os.getenv("7787780699:AAEkk7syRKGLLSWF5a_g3X-1jgC2vy61iGU")
 STRIPE_SECRET_KEY = os.getenv("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")
-STRIPE_WEBHOOK_SECRET = os.getenv("STRIPE_WEBHOOK_SECRET")
-DOMAIN = os.getenv("DOMAIN")  # tu URL en Render
+STRIPE_WEBHOOK_SECRET = os.getenv("we_1RNdBOBR7lAYvYmHo7UN4vRj")
+DOMAIN = os.getenv("https://tlgrm-losmogam-bot.onrender.com")  # tu URL en Render
